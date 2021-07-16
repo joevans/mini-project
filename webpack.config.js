eb07d9e90699e6f089346a73d08d621e117d183f
Johnathan Evans <joe.joevans@gmail.com>
2021-07-15 22:50:41 -0600

    setup complete

---
+++ webpack.config.js
@@ -0,0 +1,13 @@
+const path = require("path");
+
+module.exports = {
+	output: {
+		filename: "main.js"
+	},
+	module: {
+		rules: [{
+			test: /\.scss$/i,
+			use: [ "style-loader", "css-loader", "sass-loader" ]
+		}]
+	}
+}
